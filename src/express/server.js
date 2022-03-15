19de503cfa6b4644ccc4d6e04a47bfe7334e3132
busybox <planetgeek11@gmail.com>
2022-03-15 01:20:48 +0100

    views: Integrated basic playlist VERY UGLY WIP DO NOT USE

--- src/express/server.js
+++ src/express/server.js
@@ -5,7 +5,17 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
-app.get('*', (req, res) => {
+const spotify = require('../spotify/index')
+
+app.get('/api/spotify/getPlaylist/:id', async (req, res) => {
+	res.json(await spotify.getPlaylistFromID(req.params.id))
+})
+
+app.get('/api/spotify/getArtist/:id', async (req, res) => {
+	res.json(await spotify.getArtistFromID(req.params.id))
+})
+
+app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../../views/swipe.html'))
 })
 
--- src/spotify/index.js
+++ src/spotify/index.js
@@ -7,6 +7,23 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri: config.spotify.redirectURI
 })
 
+async function getPlaylistFromID(id) {
+    spotifyApi.setRefreshToken(config.spotify.testRefreshToken)
+    const tokenData = await spotifyApi.refreshAccessToken()
+    spotifyApi.setAccessToken(tokenData.body['access_token'])
+
+    const playlistData = await spotifyApi.getPlaylist(id)
+    return playlistData.body
+}
+
+async function getArtistFromID(id) {
+    const artistData = await spotifyApi.getArtist(id)
+    return artistData.body
+}
+
 module.exports = {
-    spotifyApi
+    spotifyApi,
+
+    getPlaylistFromID,
+    getArtistFromID
 }
